e52142b8b2168cb991949dfa67ff150ac2e6d3c4
systemengi2001-afk <systemengi2001@gmail.com>
2026-09-07 16:17:45 +0900

    fix: make sitemap static for pages export

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from 'next';
 import { articles, SITE } from '../lib/affiliateData';
 
+export const dynamic = 'force-static';
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
